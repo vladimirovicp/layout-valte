4166afb5214f40844e6c37ebd468affbb93caac2
vladimirovicp <vladimirovicp70@gmail.com>
2025-11-19 11:42:55 +0400

    feat: удаление переменных

--- vite/app.js
+++ vite/app.js
@@ -19,12 +19,9 @@ const appData = {
   APP_MODE: isProd ? 'prod' : 'dev',
 
   APP_NAME: packageData.name,
-  APP_NAME_FORMATTED: packageData.name.replace(/[^a-z]+/gi, ' ').replace(/(^\w|\s\w)/g, (m) => m.toUpperCase()),
-
+  // APP_NAME_FORMATTED: packageData.name.replace(/[^a-z]+/gi, ' ').replace(/(^\w|\s\w)/g, (m) => m.toUpperCase()),
+  APP_NAME_FORMATTED: 'vALTe',
   APP_VERSION: packageData.version,
-  APP_AUTHOR: packageData.author,
-  APP_AUTHOR_URL: packageData.authorUrl,
-  APP_REPOSITORY: packageData.repository?.url,
 
   APP_DESCRIPTION: packageData.description,
 };
